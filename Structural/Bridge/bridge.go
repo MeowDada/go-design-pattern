19e156116a21beb74dbb5b29f3690e5ba8ae54c9
MeowDada <chang.hungjui1251@gmail.com>
2020-02-04 09:33:49 +0800

    Add comments

--- Structural/Bridge/bridge.go
+++ Structural/Bridge/bridge.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 )
 
+// GreetingMethod abstracts the concept of greeting.
 type GreetingMethod interface {
 	Greet()
 }
 
+// GreetingLanguage seperates the implementation from the Greeting.
 type GreetingLanguage interface {
 	GreetWord() string
 }
 
+// English implements the GreetingLanguage interface
 type English struct {}
 
 func NewEnglish() *English { return &English{} }
@@ -20,6 +23,7 @@ func (e *English) GreetWord() string {
 	return "Hello"
 }
 
+// Chinese implements the GreetingLanguage interface
 type Chinese struct {}
 
 func NewChinese() *Chinese { return &Chinese{} }
@@ -28,10 +32,13 @@ func (c *Chinese) GreetWord() string {
 	return "你好"
 }
 
+// ViaEmail implements the GreethingMethod interface
 type ViaEmail struct {
 	GreetingLanguage
 }
 
+// NewViaEmail is a factory method to create ViaEmail object by
+// injecting an inner implementor which implements GreetingLanguage interface
 func NewViaEmail(language GreetingLanguage) *ViaEmail {
 	return &ViaEmail {
 		GreetingLanguage: language,
@@ -42,10 +49,14 @@ func (ve *ViaEmail) Greet() {
 	fmt.Printf("%s via email", ve.GreetWord())
 }
 
+// ViaTalk implements the GreethingMethod interface
 type ViaTalk struct {
 	GreetingLanguage
 }
 
+
+// NewViaTalk is a factory method to create ViaTalk object by
+// injecting an inner implementor which implements GreetingLanguage interface
 func NewViaTalk(language GreetingLanguage) *ViaTalk {
 	return &ViaTalk {
 		GreetingLanguage: language,
